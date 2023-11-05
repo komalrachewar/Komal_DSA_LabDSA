d8319ae222444d7b388ab74c6bd6aabd076cc6f7
komalrachewar <69194337+komalrachewar@users.noreply.github.com>
2023-11-05 11:37:06 +0530

    Add files via upload

---
+++ src/com/komal/main/mainDriver.java
@@ -0,0 +1,17 @@
+package com.komal.main;
+import com.komal.service.Service;
+public class mainDriver {
+    public static void main(String[] args) {
+        //String inputString1 = "( [ [ { } ] ] )";
+        //String inputString1 = "( [ [ { } ] ] ) )";
+        //String inputString1 = "( [ } { ] )";
+        //String inputString1 = "( ) [ ] { }";
+        //String inputString1 = "( { [ } ) ]";
+        String inputString1 = "( ( ( ( ( ) ) ) )";
+        if (Service.isBalancedBrackets(inputString1)) {
+            System.out.println("The entered String has Balanced Brackets");
+        } else {
+            System.out.println("The entered Strings do not contain Balanced Brackets");
+        }
+    }
+}
---
+++ src/com/komal/service/Service.java
@@ -0,0 +1,22 @@
+package com.komal.service;
+import java.util.*;
+public class Service {
+    public static boolean isBalancedBrackets(String inputString) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : inputString.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else if (ch == ')' || ch == ']' || ch == '}') {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    char top = stack.pop();
+                    if ((ch == ')' && top != '(') || (ch == ']' && top != '[') || (ch == '}' && top != '{')) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
